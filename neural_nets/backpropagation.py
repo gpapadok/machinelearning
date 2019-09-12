f4c9b14b1d8b1799c2004223fabea86367be8587
gpapadok <gpapadokostakis@gmail.com>
2019-09-12 21:12:51 +0300

    switched to numpy.exp

--- neural_nets/backpropagation.py
+++ neural_nets/backpropagation.py
@@ -1,12 +1,10 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
-from math import exp
 
 
 def sigmoid(z):
-    vexp = np.vectorize(exp)
-    return 1 / (1 + vexp(-z))
+    return 1 / (1 + np.exp(-z))
 
 def forward_propagation(X, Theta1, Theta2):
     m = X.shape[0]
